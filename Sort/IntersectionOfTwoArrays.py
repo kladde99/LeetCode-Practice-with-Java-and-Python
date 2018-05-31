8a740f311b9cfb75a3c7804e6bbf245f533672bf
kladde99 <benwanghao@126.com>
2018-05-31 10:26:11 +0800

    minor

--- Sort/IntersectionOfTwoArrays.py
+++ Sort/IntersectionOfTwoArrays.py
@@ -1,6 +1,6 @@
 '''
 No.349
-Given two arrays, write a function to compute their intersection.
+Questions： Given two arrays, write a function to compute their intersection.
 
 Example:
 Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
